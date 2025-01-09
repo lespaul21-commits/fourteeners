a801a65f9ce8c442aace73be505040a041293a55
Austin Pearman <austinpearman@Austins-MacBook-Pro.local>
2025-01-08 22:31:49 -0600

    added comments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//gets all of the peak-picture elements in an array
 let pictures = document.getElementsByClassName('peak-picture');
 
+//of accesses the values of the pictures array directly as opposed to in which accesses the indexes
 for (let picture of pictures) {
     picture.onclick = function () {
         picture.src="./Resources/keyhole.jpg"
